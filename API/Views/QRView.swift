bbfe121373380a5b8d09ea2c024dee3d0118eda6
Supakrit Nithikethkul <6410615139@cn351>
2024-03-04 13:33:04 +0700

    QR code

---
+++ API/Views/QRView.swift
@@ -0,0 +1,18 @@
+//
+//  QRView.swift
+//  API
+//
+//  Created by Supakrit Nithikethkul on 4/3/2567 BE.
+//
+
+import SwiftUI
+
+struct QRView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRView()
+}
